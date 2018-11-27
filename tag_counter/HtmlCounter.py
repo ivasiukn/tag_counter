fc16014372814aedb5329906d1bc8deb5a869de4
Nazariy Ivasyuk <ivasiukn@gmail.com>
2018-11-27 22:14:18 +0200

    print downloaded html

--- tag_counter/HtmlCounter.py
+++ tag_counter/HtmlCounter.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+import urllib.request as urllib
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+
+url = 'https://www.google.com/'
+
+response = urllib.urlopen(url)
+webContent = response.read()
+
+print(type(webContent))
+print(webContent.decode("ascii"))
+
+parser = HTMLParser()
+
+parser.
